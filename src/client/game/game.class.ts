60fdcf0b68b0146ca48e69ded285b077f3ef59b3
Stephan Giesau <5443951+pianomister@users.noreply.github.com>
2018-08-05 02:13:33 +0200

    Optimize screen size calculation for canvas

--- src/client/game/game.class.ts
+++ src/client/game/game.class.ts
@@ -10,10 +10,14 @@ export class Game {
 	private login: LoginScene;
 
 	constructor () {
+
+		// determine window size / ratio
+		const size = this.calculateGameSize();
+
 		const config: GameConfig = {
 			type: Phaser.AUTO,
-			width: window.innerWidth / 2,
-			height: window.innerHeight / 2,
+			width: size.width,
+			height: size.height,
 			'render.pixelArt': true,
 			physics: {
 				default: 'arcade',
@@ -30,6 +34,13 @@ export class Game {
 		this.game = new Phaser.Game(config);
 
 		this.login = new LoginScene(this);
+
+		/*
+		window.onresize = () => {
+			const size = this.calculateGameSize();
+			this.game.resize(size.width, size.height);
+		};
+		*/
 	}
 
 	public authenticate (name: string, character: Character) {
@@ -40,6 +51,31 @@ export class Game {
 				character
 			});
 		}
+	}
+
+	public calculateGameSize (): { width: number, height: number } {
+		let w = window,
+			d = document,
+			e = d.documentElement,
+			g = d.getElementsByTagName('body')[0],
+			clientWidth = w.innerWidth || e.clientWidth || g.clientWidth,
+			clientHeight = w.innerHeight || e.clientHeight || g.clientHeight;
+		const max1 = 1080;
+		const max2 = 600;
+		let width, height;
+
+		if (clientWidth > clientHeight) {
+			width = Math.min(clientWidth, max1);
+			height = Math.min(clientHeight, max2);
+		} else {
+			width = Math.min(clientWidth, max2);
+			height = Math.min(clientHeight, max1);
+		}
+
+		return {
+			width,
+			height
+		};
 	}
 }
 
