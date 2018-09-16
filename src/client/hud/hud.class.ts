78f960f213cfef252ffea534265a06ce3f5a4d2c
Stephan Giesau <5443951+pianomister@users.noreply.github.com>
2018-09-16 12:03:49 +0200

    Set shadow of name labels according to team color

--- src/client/hud/hud.class.ts
+++ src/client/hud/hud.class.ts
@@ -1,22 +1,25 @@
 import {Scene, Text} from '../game/types';
 import {Player} from '../actors/player/player.class';
 import {LayerDepth} from '../game/settings';
+import {TeamColors} from '../../shared/config';
 
 export class Hud {
 
 	private name: Text;
-	private style: { font, fill, align };
+	private style = {
+		font: 'bold 10px Arial',
+		fill: '#000000',
+		align: 'center',
+		stroke: '#ffffff',
+		strokeThickness: 3
+	};
 
 	constructor (scene: Scene, player: Player) {
-		this.style = {
-			font: '10px Arial',
-			fill: '#ffffff',
-			align: 'center'
-		};
-
+		this.style.stroke = `#${TeamColors[player.team].toString(16)}`;
 		this.name = scene.add.text(0, 0, player.name.substring(0, 12), this.style);
-		this.name.setOrigin(0.5, 0);
-		this.name.setDepth(LayerDepth.HUD);
+		this.name
+			.setOrigin(0.5, 0)
+			.setDepth(LayerDepth.HUD);
 		this.setCoordinates(player);
 	}
 
@@ -28,7 +31,7 @@ export class Hud {
 		}
 	}
 
-	public destroy() {
+	public destroy () {
 		this.name.destroy();
 	}
 }
